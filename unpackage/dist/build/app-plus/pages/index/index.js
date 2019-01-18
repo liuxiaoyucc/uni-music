0970fe2d78cb42694502f2d791ec507b04190f79
liuxy <ccliuxy@gmail.com>
2019-01-19 00:46:45 +0800

    android package

---
+++ unpackage/dist/build/app-plus/pages/index/index.js
@@ -0,0 +1,13 @@
+
+      !(function(){
+        var uniAppViewReadyCallback = function(){
+          setCssToHead([".",[1],"content.",[1],"data-v-6e0b790e { text-align: center; height: ",[0,400],"; }\n.",[1],"logo.",[1],"data-v-6e0b790e{ height: ",[0,200],"; width: ",[0,200],"; margin-top: ",[0,200],"; }\n.",[1],"title.",[1],"data-v-6e0b790e { font-size: ",[0,36],"; color: #8f8f94; }\n",],undefined,{path:"./pages/index/index.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/index/index.wxml') } }));
+        }
+        if(window.__uniAppViewReady__){
+          uniAppViewReadyCallback()
+        }else{
+          document.addEventListener('uniAppViewReady',uniAppViewReadyCallback)
+        }
+      })();
+      
