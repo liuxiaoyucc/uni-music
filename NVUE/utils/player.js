738b1fb49672a7e7fe6432abb1e3c4321ce26cc8
xyliu <2199842341@qq.com>
2020-02-11 01:12:14 +0800

    播放暂停

--- NVUE/utils/player.js
+++ NVUE/utils/player.js
@@ -3,11 +3,22 @@ let PlayerHelper = {};
 
 let player = getApp({allowDefault: true}).globalData.player;
 
-PlayerHelper.start = function(url, cb) {
+PlayerHelper.start = function(url) {
+	console.log(2);
 	console.log('play start');
 	player.src = url;
 	player.play();
-	cb && cb(true);
+	return;
+}
+
+/**
+ * 暂停
+ * 需要start后, 触发onPlay事件后, pause才有效
+ */
+PlayerHelper.pause = function() {
+	console.log('play pause');
+	player.pause();
+	return;
 }
 
 module.exports = PlayerHelper
